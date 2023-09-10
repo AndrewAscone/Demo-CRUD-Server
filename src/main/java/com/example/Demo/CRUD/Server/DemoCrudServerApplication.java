e5098c02c0b7dc6798f556c43f0b3d4e05124107
AndrewAscone <andrew.ascone@gmail.com>
2023-09-10 00:38:27 -0400

    First commit

---
+++ src/main/java/com/example/Demo/CRUD/Server/DemoCrudServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demo.CRUD.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudServerApplication.class, args);
+	}
+
+}
